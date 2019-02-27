3e333f77a5fad781a022c35936d5cd98f1d3ba64
Gloria <wlin17@sheffield.ac.uk>
2019-02-27 19:16:16 +0000

    Updated the web content by incorporating the template

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # create a new copy of a Flask app to work with
 
-@app.route("/")
+@app.route("/") 
+# decorate the function such that it can respond to request at the http address
+# located before a funtion
+# / = localhost:5000/
 def say_hello():
-  return "hello world!"
+  # return "hello world!"
+  return render_template("index.html") # update
 
-app.run(debug=True)
+app.run(debug=True) 
+# starts up a web server controllable by Flask
+# debug=True - automatically restart the program each time we made changes to our Python code
